7ad75d380905092f5adfa14f5ebffce10c3cb9e8
felixjimcal <felixjcalvo@gmail.com>
2019-08-22 15:37:44 +0200

    Calc done, time obtained

--- app/src/main/java/felix/rmspeed/MainActivity.java
+++ app/src/main/java/felix/rmspeed/MainActivity.java
@@ -1,30 +1,35 @@
 package felix.rmspeed;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.MediaController;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.VideoView;
-
-import java.util.concurrent.TimeUnit;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int READ_REQUEST_CODE = 42;
-    private static final int MIN_VIDEO_THUMB_POSITION = 0;
-    private static int VIDEO_FPS = 100;
-    private static final String FILE_TYPE_TO_FIND = "video/*";
+    private static final int MIN_FRAME = 0;
+    private static final String FILE_TYPE = "video/*";
+
+    private int TOTAL_FRAMES = 0, START_FRAME = 0, END_FRAME = 0, VIDEO_DURATION = 0;
 
     private Button btnLoadVideo, btnLess, btnMore, btnStart, btnEnd;
-    private VideoView videoView;
+    private ImageView imageView;
+    private SeekBar seekBar;
+    private MediaMetadataRetriever mediaMetadataRetriever;
     private TextView txtTime;
 
     @Override
@@ -34,24 +39,23 @@ public class MainActivity extends AppCompatActivity {
 
         ImportReferences();
 
-        btnLoadVideo.setOnClickListener(new View.OnClickListener()
-        {
+        btnLoadVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType(FILE_TYPE_TO_FIND);
+                intent.setType(FILE_TYPE);
 
-                try
-                {
+                try {
                     startActivityForResult(intent, READ_REQUEST_CODE);
-                } catch (ActivityNotFoundException e){
+                } catch (ActivityNotFoundException e) {
                     Toast.makeText(MainActivity.this, "There are no file explorer clients installed.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
         btnLess.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
             @Override
             public void onClick(View v) {
                 AdjustThumbPositionInVideo(FPS.LESS);
@@ -59,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnMore.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
             @Override
             public void onClick(View v) {
                 AdjustThumbPositionInVideo(FPS.MORE);
@@ -68,99 +73,120 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnStart.setText(String.valueOf(videoView.getCurrentPosition()));
+                START_FRAME = seekBar.getProgress();
             }
         });
 
         btnEnd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnEnd.setText(String.valueOf(videoView.getCurrentPosition()));
+                END_FRAME = seekBar.getProgress();
                 CalculateTime();
             }
         });
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int actualFrame = 0;
 
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                txtTime.setText(String.valueOf(progress));
+                actualFrame = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.P)
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                SetFrame(actualFrame);
+            }
+        });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            Uri uri = null;
-            if (resultData != null)
-            {
-                SetVideoOnVideoView(resultData);
+            if (resultData != null) {
+                SettingUpMedia(resultData);
 
-                SetVideoBar();
+                SettingUpSeekBar();
+
+                SetFrame(MIN_FRAME);
             }
         }
     }
 
-    private void SetVideoBar() {
-        MediaController mediaController = new MediaController(videoView.getContext());
-        videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
+    private void SettingUpSeekBar() {
+        TOTAL_FRAMES = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_FRAME_COUNT));
+        seekBar.setMax(TOTAL_FRAMES);
     }
 
-    private void SetVideoOnVideoView(Intent resultData) {
+    private void SettingUpMedia(Intent resultData) {
         Uri uri = resultData.getData();
-        videoView.setVideoURI(uri);
-        videoView.start();
+        mediaMetadataRetriever.setDataSource(getApplicationContext(), uri);
+
+        VIDEO_DURATION = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
     }
 
-    private void AdjustThumbPositionInVideo(FPS frames)
-    {
-        if(videoView.getCurrentPosition() > MIN_VIDEO_THUMB_POSITION && videoView.getCurrentPosition() < videoView.getDuration())
-        {
-            videoView.pause();
+    @SuppressLint("ShowToast")
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void SetFrame(int frame) {
+        try {
+            seekBar.setProgress(frame);
 
-            int position = videoView.getCurrentPosition();
-
-            switch(frames)
-            {
-                case LESS:
-                    position -= VIDEO_FPS;
-                    break;
-
-                case MORE:
-                    position += VIDEO_FPS;
-                    break;
-            }
-
-            try
-            {
-                videoView.seekTo(position);
-            }
-            catch (Exception exception)
-            {
-                System.out.println("Exception:!!!: " + exception.getMessage());
-            }
+            Bitmap bitmap = mediaMetadataRetriever.getFrameAtIndex(frame);
+            imageView.setImageBitmap(bitmap);
+        } catch (Exception exception) {
+            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG);
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void AdjustThumbPositionInVideo(FPS frames) {
+        int actualFrame = seekBar.getProgress();
+        switch (frames) {
+            case LESS:
+                if (actualFrame != 0) {
+                    actualFrame--;
+                }
+                break;
+
+            case MORE:
+                if (actualFrame < TOTAL_FRAMES) {
+                    actualFrame++;
+                }
+                break;
+        }
+
+        SetFrame(actualFrame);
     }
 
     public void CalculateTime()
     {
-        try
-        {
-            int val1 = btnStart.getText() != null ? Integer.valueOf((String) btnStart.getText()) : 0;
-            int val2 = btnEnd.getText() != null ? Integer.valueOf((String) btnEnd.getText()) : 0;
-            int resuklt = val2 - val1;
+        if (START_FRAME > END_FRAME || START_FRAME == END_FRAME) {
+            txtTime.setText("Nope");
+        } else {
+            int frames = END_FRAME - START_FRAME;
+            float result = ( VIDEO_DURATION * frames) / TOTAL_FRAMES;
 
-            txtTime.setText(String.valueOf(TimeUnit.MILLISECONDS.toSeconds(resuklt)));
-        }
-        catch (Exception exception)
-        {
-            System.out.println("Exception:!!!: " + exception.getMessage());
+            txtTime.setText(String.valueOf(result));
         }
     }
 
     private void ImportReferences() {
         btnLoadVideo = findViewById(R.id.btnLoadVideo);
+        imageView = findViewById(R.id.imageView);
+        mediaMetadataRetriever = new MediaMetadataRetriever();
 
         btnLess = findViewById(R.id.btn_less);
         btnMore = findViewById(R.id.btn_more);
 
-        videoView = findViewById(R.id.vView);
+        seekBar = findViewById(R.id.seekBar2);
 
         btnStart = findViewById(R.id.btnStart);
         txtTime = findViewById(R.id.txtTime);
